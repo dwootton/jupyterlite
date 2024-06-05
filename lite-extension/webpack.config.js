143e235c66e4ad13971b1d990d72ee090c13bd95
Dylan Wootton <wootton.dylan@gmail.com>
2024-06-05 18:17:30 -0400

    added lite-lib to webpack

--- lite-extension/webpack.config.js
+++ lite-extension/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'lib'),
+    path: path.resolve(__dirname, 'lite-lib'),
     libraryTarget: 'umd'
   },
   module: {
