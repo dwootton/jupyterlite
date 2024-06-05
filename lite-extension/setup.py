b63b3f7dcc0a9ff1a9cbf779e3522fbeee0effa2
Dylan Wootton <wootton.dylan@gmail.com>
2024-06-05 17:57:52 -0400

    Add save to URL button extension

---
+++ lite-extension/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lite-extension',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    data_files=[
+        ('share/jupyter/labextensions/lite-extension', [
+            'lib/index.js'
+        ])
+    ],
+    zip_safe=False,
+    install_requires=[
+        'jupyterlite',
+        'jupyterlab'
+    ],
+    entry_points={},
+)
